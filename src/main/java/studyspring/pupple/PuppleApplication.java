462e9908ca2cd59b9bf19d3c3babe9eae86f610f
yujin <dksdbwls15@naver.com>
2021-12-02 18:57:00 +0900

    Initial commit

---
+++ src/main/java/studyspring/pupple/PuppleApplication.java
@@ -0,0 +1,13 @@
+package studyspring.pupple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppleApplication.class, args);
+	}
+
+}
